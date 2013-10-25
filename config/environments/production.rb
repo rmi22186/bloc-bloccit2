02054375d74ea480f7d04bd0a887be26eb8f596b
Robert Ing <robertming@gmail.com>
2013-10-24 17:55:29 -0700

    Re-configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Bloccit1::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'http://stormy-fortress-3214.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://stormy-fortress-3214.herokuapp.com' }
 end

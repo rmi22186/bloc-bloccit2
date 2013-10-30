52aa01b586622fcb4959a65d5f359c792930ef0b
Robert Ing <robertming@gmail.com>
2013-10-30 14:29:21 -0700

    Add facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Bloccit1::Application.routes.draw do
   
   get "comments/create"
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } #This declaration instructs the redirect to look for a controller in the users/ directory, by the name of omniauth_callbacks_controller.rb [used when signing up with facebook]
 
   resources :topics do
     resources :posts, except: [:index] do

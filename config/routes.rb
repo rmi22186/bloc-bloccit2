dff477414124a7ec5b16c018caa74dc684972998
Robert Ing <robertming@gmail.com>
2013-11-04 13:00:40 -0800

    Created popular posts and users, optimized topics#show with eager loading.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Bloccit1::Application.routes.draw do
   
+  get "posts/index"
+
   get "comments/create"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' } #This declaration instructs the redirect to look for a controller in the users/ directory, by the name of omniauth_callbacks_controller.rb [used when signing up with facebook]
 
-  resources :users, only: [:show] #create a route for users#show
+  
+  resources :posts, only: [:index]
+  resources :users, only: [:show, :index] #create a route for users#show
   resources :topics do #does this format always set the below resource to be a subcategory of the above one?
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
